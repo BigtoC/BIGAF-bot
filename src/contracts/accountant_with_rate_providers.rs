8811319ffc19296ed03b22eebab3f87d36e292ea
BigtoC <taitocdt@gmail.com>
2025-11-20 12:27:36 +0800

    feat(contract): refactor AccountantWithRateProviders and add ERC20 contract

--- src/contracts/accountant_with_rate_providers.rs
+++ src/contracts/accountant_with_rate_providers.rs
@@ -1,14 +1,8 @@
 use alloy::sol;
-use alloy_primitives::Address;
 
 sol! {
     #[sol(rpc)]
     contract AccountantWithRateProviders {
-        function getRateSafe() external view returns (uint256 rate);
+        function getRate() public view returns (uint256);
     }
-}
-
-pub struct AccountantWithRateProvidersContract {
-    address: Address,
-    rpc_url: String,
 }
--- src/contracts/simplified_teller.rs
+++ src/contracts/simplified_teller.rs
@@ -1,5 +1,4 @@
 use alloy::sol;
-use alloy_primitives::Address;
 
 sol! {
     #[sol(rpc)]
@@ -7,9 +6,4 @@ sol! {
         function deposit(address depositAsset, uint256 depositAmount, uint256 minimumMint);
         function withdraw(address withdrawAsset, uint256 shareAmount, uint256 minimumAssets);
     }
-}
-
-pub struct SimplifiedTellerContract {
-    address: Address,
-    rpc_url: String,
 }
